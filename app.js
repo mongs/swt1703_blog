d8eee4aff249c05fb4b61e02305d7e459dfa5d29
mongos <291767002@qq.com>
2017-07-18 11:50:49 +0800

    session存储管理登录状态 types_add articles_add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -21,6 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'sram wally',
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.use('/', index);
 app.use('/admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
 /**
- * admin 用户列表页
+ * 用户列表页
  */
 router.get('/users', function(req, res) {
+    if(!req.session.username){
+        res.redirect('/admin/login')
+    }
     res.render('back/users')
 });
 /**
@@ -14,10 +17,22 @@ router.get('/types', function(req, res) {
     res.render('back/types')
 });
 /**
+ * 分类 添加页
+ */
+router.get('/types_add', function(req, res) {
+    res.render('back/types_add')
+});
+/**
  * 文章 列表页
  */
 router.get('/articles', function(req, res) {
     res.render('back/articles')
+});
+/**
+ * 文章 添加页
+ */
+router.get('/articles_add', function(req, res) {
+    res.render('back/articles_add')
 });
 /**
  * 渲染登录界面
@@ -40,6 +55,8 @@ router.post('/login', (req, res) => {
       password: password
   }).then(result => {
     if(result){
+        req.session.cookie.maxAge = 3000000;
+        req.session.username = username;
         //登录成功
         res.json({
             status: 0,
